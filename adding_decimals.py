2adac2adbf1808f4e8d1369277053b56f149a1cf
Andrew-Ten-Den <andrew.ten-den@mths.ca>
2022-03-10 19:38:02 +0000

    added decimals

---
+++ adding_decimals.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created by: Andrew-Ten-Den
+# Created on: March, 2022
+# This program calculates 0,1 + 0.2
+
+
+def main():
+    # This program calculates 0.1 + 0.2
+    print("0.1 + 0.2 = {}".format(0.1 + 0.2))
+    print("")
+    print("Done")
+
+
+if __name__ == "__main__":
+    main()
